91ee43124d529c91d068e5f84cd5d7311e126f1a
Sanket Patil <sanket.patil@bluepineapple.io>
2023-05-26 16:02:16 +0530

    Added apis

---
+++ src/components/weather/index.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Weather = () => {
+    const [weatherData, setWeatherData] = useState(null);
+    const API_KEY = '68e1864303dae3362322d5ee63e92950';
+    const location = 'Belgaum';
+
+    useEffect(() => {
+        const fetchWeatherData = async () => {
+            try {
+                const response = await axios.get(
+                    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}`
+                );
+                setWeatherData(response.data);
+            } catch (error) {
+                console.error('Error fetching weather data:', error);
+            }
+        };
+
+        fetchWeatherData();
+    }, [location]);
+
+    if (!weatherData) {
+        return <div>Loading weather data...</div>;
+    }
+
+    return (
+        <div>
+            <h2>{location}</h2>
+            <p>Temperature: {weatherData.main.temp}</p>
+            <p>Humidity: {weatherData.main.humidity}</p>
+            <p>Air Quality: {weatherData.airQuality}</p>
+            <p>Wind: {weatherData.wind.speed}</p>
+            <p>Wind Gusts: {weatherData.wind.gust}</p>
+        </div>
+    );
+};
+
+export default Weather;
